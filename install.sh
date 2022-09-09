3b076cec0f8c6b56e2b2f92ddc21c77c556b7436
Rômulo Meira-Góes <romulo@umich.edu>
2022-09-09 10:29:47 -0400

    Adding a readme and requirements to install

--- install.sh
+++ install.sh
@@ -6,21 +6,6 @@ echo $SCRIPT_DIR
 
 sudo dpkg -i requirements/*.deb
 
-MATHSATLIB_DIR="${SCRIPT_DIR}/lib/mathsat-5.6.6/" 
-echo $MATHSATLIB_DIR
-sudo ln -sf "${MATHSATLIB_DIR}/include/mathsat.h" /usr/local/include/mathsat.h
-sudo ln -sf "${MATHSATLIB_DIR}/include/mathsat.h" /usr/local/include/mathsatll.h
-sudo ln -sf "${MATHSATLIB_DIR}/include/mathsataig.h" /usr/local/include/mathsataig.h
-sudo ln -sf "${MATHSATLIB_DIR}/lib/libmathsat.a" /usr/local/lib/libmathsat.a
-sudo ln -sf "${MATHSATLIB_DIR}/lib/libmathsat.so" /usr/local/lib/libmathsat.so
-
-
-cd lib/FuseIC3
-mkdir build
-cd build
-cmake ..
-make
-
 cd ../../..
 python3 -m venv venv-tolerancetool
 source venv-tolerancetool/bin/activate
